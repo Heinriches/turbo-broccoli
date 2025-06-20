c3656a8b2f61c3ad8a85351bf2e422e3ca37ffb4
Heinriches <heydrich.institut@gmail.com>
2025-06-20 12:38:44 -0300

    created autenticar.php

--- portalead/sistema/index.php
+++ portalead/sistema/index.php
@@ -1,1 +1,19 @@
-portal do sistema
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Portal Hugo Cursos</title>
+	</head>
+
+	<body>
+		<form method="POST" action="autenticar.php">
+			<input type="text" name="usuario" placeholder="E-mail ou CPF">
+			<input type="password" name="senha" placeholder="Digite a sua senha">
+			<button type="submit">Logar</button>
+		</form>
+
+	</body>
+
+</html>
